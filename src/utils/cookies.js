b0ad92d7f9f7ab3c18b2bb9a8669b59b94e2591e
iamsreehari02 <sreeharivnambiar72@gmail.com>
2025-06-03 02:45:54 +0530

    fixes

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -3,7 +3,7 @@ export const setAuthCookie = (res, token) => {
     httpOnly: true,
     // secure: process.env.NODE_ENV === "production",
     secure: false,
-    sameSite: "Lax",
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
